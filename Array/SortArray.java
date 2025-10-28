0efd93742b31ebf792160dd6ad430b5d49257fa1
HoangBV <hoangga714@gmail.com>
2025-10-28 22:28:23 +0700

    updated Array ex

---
+++ Array/SortArray.java
@@ -0,0 +1,40 @@
+package Array;
+
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] Numbers = { 5, 7, 20, 2, 5 };
+        sortArrays(Numbers);
+        System.out.println("\n" + Arrays.toString(Numbers));
+        int balance = 10;
+        System.out.println("Balance is " + checkingPalindromes(balance));
+    }
+
+    private static void sortArrays(int[] Numbers) {
+
+        for (int i = 0; i < Numbers.length - 1; i++) {
+            for (int j = i + 1; j < Numbers.length; j++) {
+                if (Numbers[i] <= Numbers[j]) {
+                    // do nothing
+                } else {
+                    int temp = Numbers[j];
+                    Numbers[j] = Numbers[i];
+                    Numbers[i] = temp;
+                }
+
+            }
+
+        }
+    }
+
+    private static int checkingPalindromes(int balance) {
+
+        while (true) {
+            if (balance < 9)
+                break;
+            balance = balance - 9;
+        }
+        return balance;
+    }
+}
