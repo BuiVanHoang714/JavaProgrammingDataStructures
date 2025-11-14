bd4a35598324fdab50e86734c40d6d2e7b174c97
HoangBV <hoangga714@gmail.com>
2025-11-14 21:46:33 +0700

    Refactor TwoSum class: fix syntax error and improve method visibility

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -1,13 +1,15 @@
 
+package Array;
+
 public class TwoSum {
     public static void main(String[] args) {
-        int[] example = {1,2,3,4};
+        int[] example = { 1, 2, 3, 4 };
         // Example: Assign grades based on scores
-        System.out.println((twoSum(example, 3).));
+        System.out.println(java.util.Arrays.toString(Solution.twoSum(example, 3)));
     }
 
-    class Solution {
-        public int[] twoSum(int[] nums, int target) {
+    private static class Solution {
+        public static int[] twoSum(int[] nums, int target) {
             int[] result = new int[2];
             for (int i = 0; i < nums.length; i++) {
                 for (int j = i + 1; j < nums.length; j++) {
